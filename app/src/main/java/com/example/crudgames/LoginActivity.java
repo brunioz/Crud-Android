482cd20c783f7b95c83d350e5996f8437f01c209
Bruno Guedes <bruniozguedes@gmail.com>
2022-09-14 02:13:20 -0300

    Novas funcionalidades

---
+++ app/src/main/java/com/example/crudgames/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.crudgames;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText user;
+    EditText senha;
+    Button btnLogin;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        user = (EditText) findViewById(R.id.user);
+        senha = (EditText) findViewById(R.id.senha);
+        btnLogin = (Button) findViewById(R.id.btn_entrar);
+        btnLogin.setOnClickListener((View) -> {
+
+            if(user.getText().toString().isEmpty() || senha.getText().toString().isEmpty()){
+
+                Toast.makeText(getApplicationContext(), "Por favor preencha todos os campos", Toast.LENGTH_LONG).show();
+                
+            } else {
+
+                if (user.getText().toString().equals("admin") && senha.getText().toString().equals("123")) {
+                    startActivity(new Intent(this, ListarJogadoresActivity.class));
+                } else {
+                    Toast.makeText(this, "Usuario ou senha inválidos", Toast.LENGTH_SHORT).show();
+                }
+                
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/com/example/crudgames/SplashApresentacao.java
@@ -0,0 +1,31 @@
+package com.example.crudgames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashApresentacao extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_apresentacao);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                    Intent i = new Intent(SplashApresentacao.this, Apresentacao.class);
+                    startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
